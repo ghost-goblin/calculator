c5c387a60a67ab1817d168ba6a68dd3f787ccec6
Chloe Urisohn <chloeurisohn@gmail.com>
2021-01-10 12:13:22 +0000

    Add Operator Functions

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const nums = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('button');
+
+// Operate Functions
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return (a * b);
+};
+
+function divide(a, b) {
+    return (a / b);
+};
 
 
-nums.forEach((num => {
-    console.log(num.value);
 
-}))
+buttons.forEach((button => {
+    button.addEventListener('click', operate);
+}));
+
+function operate(e) {
+    operator = e.target.getAttribute('value');
+    a = 4;
+    b = 5;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            console.log(result);
+            break;
+        case '-':
+            console.log('minus');
+            break;
+
+    };
+};
