96fc27c28ea6e85a96f04646f14fbbbe94ab134f
Chloe Urisohn <chloeurisohn@gmail.com>
2021-01-13 14:45:40 +0000

    Add Calculator class and oop

--- script.js
+++ script.js
@@ -1,89 +1,100 @@
-const buttons = document.querySelectorAll('button');
-const display = document.querySelector('.display');
-const clear = document.querySelector('.ac');
-const equals = document.querySelector('.equal-sign');
-const decimal = /^(\d*\.)?\d+$/g; // decimal regex
-
-
-const clearDisplay = () => {
-    display.textContent = '';
-};
-
-// Operate Functions
-function add(a, b) {
-    return a + b;
-};
-
-function subtract(a, b) {
-    return (a - b);
-};
-
-function multiply(a, b) {
-    return (a * b);
-};
-
-function divide(a, b) {
-    return (a / b);
-};
-
-// Event Listeners
-buttons.forEach((button => {
-    button.addEventListener('click', calculate);
-}));
-
-function calculate(e) {
-    if ((e.target.className !== 'operator') 
-    && (e.target.getAttribute('value') !== '=')
-    && (display.textContent.length < 15)) {
-        displayValue = e.target.getAttribute('value');
-        display.textContent += displayValue;
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
     };
-    // Clear the Display ...
-    if (e.target.className === 'ac') {
-        clearDisplay();
-    };
-};
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operation = undefined;
 
-// Get Operator & Operands Value/s ...
-buttons.forEach((button => {
-    button.addEventListener('click', function() {
-        if (button.className === 'operator') {
-            a = Number(display.textContent);
-            display.textContent = '';
-            operator = button.value;
+    };
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;   //  stops the return of double periods
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    };
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return; // skip if no operand is available
+        if (this.previousOperand !== '') {
+            this.calculate();
         };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    };
+
+    calculate() {
+        let calculation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        console.log(prev + this.operation + this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch(this.operation) {
+            case '+':
+                calculation = prev + current;
+                console.log(prev + current);
+                break;
+            case '-':
+                calculation = prev - current;
+                console.log(prev - current);
+                break;
+            case '*':
+                calculation = prev * current;
+                console.log(prev * current);
+                break;
+            case '/':
+                calculation = prev / current;
+                console.log(prev / current);
+                break;
+            default:
+                return;
+        };
+        this.currentOperand = calculation;
+        console.log(calculation);
+        this.operation = undefined;
+        this.previousOperand = '';
+    };
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = this.previousOperand;
+    };
+};
+
+// Button Variables
+const digitButtons = document.querySelectorAll('[data-digit]');
+const operationButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+digitButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
     });
-}));
+});
 
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.value);
+        calculator.updateDisplay();
+    });
+});
 
-// Get Result ...
-equals.addEventListener('click', function () {
-    b = Number(display.textContent);
-    operate(a, b, operator);
+equalsButton.addEventListener('click', button => {
+    calculator.calculate();
+    calculator.updateDisplay();
 });
 
 
-// Operate Function
-function operate(a, b, operator) {
-    switch (operator) {
-        case '+':
-            result = add(a, b);
-            display.innerText = result;
-            break;
-        case '-':
-            result = subtract(a, b);
-            display.innerText = result;
-            break;
-        case '*':
-            result = multiply(a, b);
-            display.innerText = result;
-            break;
-        case '/':
-            result = divide(a, b);
-            display.innerText = result;
-            break;
-        default:
-            console.log('IDK...');
-    };
-    console.log(result);
-};
+clearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
